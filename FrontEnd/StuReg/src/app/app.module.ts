eabb97fec1e55aaad75713fdcc0974d03b92d97d
srimathi78 <srimathipage@gmail.com>
2023-06-27 03:07:39 -0400

    first commit

---
+++ FrontEnd/StuReg/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { StudentDeleteComponent } from './student-delete/student-delete.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentService } from './student.service';
+import { StudentComponent } from './student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,   
+    StudentEditComponent,   
+    StudentDeleteComponent 
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [StudentService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
